a614591a7d13b0b25ba0039dc272c069532f77e9
DevPolo <paul.crussaire@gmail.com>
2021-11-10 11:31:15 +0100

    feat(✅): setup db and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { join } from 'path';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 import { RoomsModule } from './rooms/rooms.module';
 
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.DATABASE_URL,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     RoomsModule,
   ],
