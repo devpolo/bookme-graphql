cc10640cc77cdf3fe0ba06c2afb3e839dea21970
DevPolo <paul.crussaire@gmail.com>
2021-11-10 11:52:56 +0100

    fix(🐛): mongodb version

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -10,10 +10,6 @@ export class RoomsService {
   constructor(@InjectRepository(Room) private roomRepository: Repository<Room>) {}
 
   async findAll(): Promise<Room[]> {
-    const room = new Room();
-    room.id = 0;
-    room.name = 'C01';
-
-    return [room];
+    return this.roomRepository.find();
   }
 }
