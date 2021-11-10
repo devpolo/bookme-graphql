bad21022c556f49c87d571d5338bbbafc04b9dc8
DevPolo <paul.crussaire@gmail.com>
2021-11-10 10:03:14 +0100

    style(🎨): import lines

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { RoomsModule } from './rooms/rooms.module';
+
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
