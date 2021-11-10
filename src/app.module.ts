a1e4ea279762c23a8cd3e6fbee29ddf839cf1e09
DevPolo <paul.crussaire@gmail.com>
2021-11-10 10:02:24 +0100

    chore(📦): generate rooms module, service and resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    RoomsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
