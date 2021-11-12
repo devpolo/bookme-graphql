b9943a626c44dbae0ac44801173ae8f7cde975bb
DevPolo <paul.crussaire@gmail.com>
2021-11-12 14:39:18 +0100

    feat(✅): user and booking relation

--- src/booking/entities/booking.entity.ts
+++ src/booking/entities/booking.entity.ts
@@ -2,6 +2,7 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { Room } from 'src/room/entities/room.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Entity()
 @ObjectType()
@@ -29,4 +30,8 @@ export class Booking {
   @ManyToOne(() => Room, (room) => room.bookings, { onDelete: 'CASCADE' })
   @Field((type) => Room)
   room: Room;
+
+  @ManyToOne(() => User, (user) => user.bookings, { onDelete: 'CASCADE' })
+  @Field((type) => User)
+  user: User;
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,8 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { IsNotEmpty, Length } from 'class-validator';
+import { Booking } from 'src/booking/entities/booking.entity';
 
-import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from 'typeorm';
 
 @Entity()
 @Unique(['name'])
@@ -16,4 +17,8 @@ export class User {
   @Length(2, 30, { message: 'The name must be at least 2 but not longer than 30 characters' })
   @IsNotEmpty({ message: 'The name is required' })
   name!: string;
+
+  @OneToMany(() => Booking, (booking) => booking.room, { onDelete: 'SET NULL' })
+  @Field((type) => [Booking], { nullable: true })
+  bookings: Booking[];
 }
